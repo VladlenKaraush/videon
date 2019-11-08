6f69bb302b085070997574d3745064db8645deb9
vladlen <vkaraush@stream.ru>
2019-11-08 16:33:10 +0300

    deleted test route

--- backend/routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.render("index", { title: "express app", message: "Hi" });
-});
-
-module.exports = router;

85e88ffeac475491833c779ff4bf8b4d1eef78c8
Vladlen <vladlenkaraush@gmail.com>
2019-11-09 11:57:02 +0300

    fix

--- init.sh
+++ init.sh
@@ -2,3 +2,10 @@ mongo vidly --eval 'db.movies.drop()'
 mongo vidly --eval 'db.genres.drop()'
 mongoimport --db vidly --collection movies backend/db_init/movies.json
 mongoimport --db vidly --collection genres backend/db_init/genres.json
+cd backend
+npm i
+export PRIVATE_KEY=12345
+
+cd ../frontend
+npm i
+cd ..
--- startup.sh
+++ startup.sh
@@ -1,7 +1,4 @@
 cd backend
-npm i
-export PRIVATE_KEY=INSERT_YOUR_KEY
 node app.js &
-
 cd ../frontend
 npm start
